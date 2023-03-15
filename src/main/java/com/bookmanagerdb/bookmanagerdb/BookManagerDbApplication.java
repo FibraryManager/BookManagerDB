b3adf4b85d8689a74e9fa0c1667fb72259039daa
yunduo <2992588618@qq.com>
2023-03-15 16:57:03 +0800

    基础设置

---
+++ src/main/java/com/bookmanagerdb/bookmanagerdb/BookManagerDbApplication.java
@@ -0,0 +1,19 @@
+package com.bookmanagerdb.bookmanagerdb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@ComponentScan(basePackages = {"com.bookmanagerdb"})
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableWebSecurity
+public class BookManagerDbApplication {
+
+    public static void main(String[] args) {
+SpringApplication.run(BookManagerDbApplication.class, args);
+}
+
+}
